052cf0ff589557488ec349440712766b9bf7931f
lxc <3250162035@qq.com>
2025-08-11 22:53:29 +0800

    init

---
+++ src/main/java/io/github/timemachinelab/promptolab/PromptoLabApplication.java
@@ -0,0 +1,13 @@
+package io.github.timemachinelab.promptolab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptoLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromptoLabApplication.class, args);
+    }
+
+}
